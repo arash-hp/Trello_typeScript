6ae0622d58971fcc0f61a7cefb7080efc66424f9
Arash Hp <arash.hpb@gamil.com>
2022-12-25 09:10:43 +0330

    Add layout for dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -2,25 +2,26 @@ import { Grid } from "@mui/material";
 import { FC } from "react";
 import BoardContent from "../components/BoardContent";
 import image from "../assets/image/bakgroundSample.jpg";
-
-
-
-
+import MainLayout from "../layouts/MainLayout";
 
 const DashboardPage: FC = () => {
+  const handelClick = () => { };
 
-// TODO : ObjectClassName
+  // TODO : ObjectClassName
   return (
-    <Grid
-      sx={{
-        backgroundImage:
-          `url(${image})`,
-        height: "100vh",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}>
-      <BoardContent />
-    </Grid>
+    <MainLayout onClick={handelClick}>
+
+      <Grid
+        sx={{
+          backgroundImage:
+            `url(${image})`,
+          height: "100vh",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}>
+        <BoardContent />
+      </Grid>
+    </  MainLayout>
   );
 };
 
